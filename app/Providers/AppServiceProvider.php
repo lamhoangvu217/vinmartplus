d535228e8052696d45b2987d7a711cf6d94c6326
lamhoangvu217 <lamhoangvu217@gmail.com>
2021-10-31 17:04:14 +0700

    sua doi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if (env('APP_ENV') == 'Production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
